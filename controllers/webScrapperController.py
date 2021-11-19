f089667ff054de2832cc931709aadff695bc30e5
Guilherme Ely <guilherme.ely.ferreira@gmail.com>
2021-11-19 13:18:21 -0300

    webScrapper class

---
+++ controllers/webScrapperController.py
@@ -0,0 +1,46 @@
+from bs4 import BeautifulSoup
+import requests
+import re
+import datetime as dt
+
+class WebScrapperController:
+
+    def __init__(self):
+        pass
+
+    def convertTimestamp(self, timestamp):
+        datetimeValues = []
+        for ts in timestamp:
+            datetimeValues.append(dt.datetime.utcfromtimestamp(ts/1000))
+
+        return datetimeValues
+
+    def getRedditCryptoData(self, crypto):
+        url = 'https://apewisdom.io/cryptocurrencies/'+crypto+'/'
+        soup = BeautifulSoup(requests.get(url).text, 'html.parser')
+
+        #Get the data thar is how many people are talking about the crypto
+        itemData = soup.find("script", text=re.compile(r'\bdata:\s*\['))
+
+        matchData = re.findall(r'\bdata:\s*\[([^][]*)]', itemData.string)
+
+        if matchData:
+            dataAll = map(int, matchData[0].split(','))
+            dataCryptoCurrencySub = map(int, matchData[1].split(','))
+
+        #Get the labels, which are the datetime
+        itemLabels = soup.find("script", text=re.compile(r'\blabels:\s*\['))
+
+        matchLabels = re.findall(r'\blabels:\s*\[([^][]*)]', itemLabels.string)
+
+        if matchLabels:
+            labelsAll = map(int, matchLabels[0].split(','))
+            labelsCryptoCurrencySub = map(int, matchLabels[1].split(','))
+
+        labelsAll = self.convertTimestamp(labelsAll)
+        labelsCryptoCurrencySub = self.convertTimestamp(labelsCryptoCurrencySub)
+
+        return dataAll, dataCryptoCurrencySub, labelsAll, labelsCryptoCurrencySub
+
+    def __del__(self):
+        pass
--- index.py
+++ index.py
@@ -1,49 +1,15 @@
-from bs4 import BeautifulSoup
-import requests
-import re
-import datetime as dt
 import pandas as pd
 import matplotlib.pyplot as plt
-
-def convertTimestamp(timestamp):
-    datetimeValues = []
-    for ts in timestamp:
-        datetimeValues.append(dt.datetime.utcfromtimestamp(ts/1000))
-
-    return datetimeValues
-
-def getRedditCryptoData(crypto):
-    url = 'https://apewisdom.io/cryptocurrencies/'+crypto+'/'
-    soup = BeautifulSoup(requests.get(url).text, 'html.parser')
-
-    #Get the data thar is how many people are talking about the crypto
-    itemData = soup.find("script", text=re.compile(r'\bdata:\s*\['))
-
-    matchData = re.findall(r'\bdata:\s*\[([^][]*)]', itemData.string)
-
-    if matchData:
-        dataAll = map(int, matchData[0].split(','))
-        dataCryptoCurrencySub = map(int, matchData[1].split(','))
-
-    #Get the labels, which are the datetime
-    itemLabels = soup.find("script", text=re.compile(r'\blabels:\s*\['))
-
-    matchLabels = re.findall(r'\blabels:\s*\[([^][]*)]', itemLabels.string)
-
-    if matchLabels:
-        labelsAll = map(int, matchLabels[0].split(','))
-        labelsCryptoCurrencySub = map(int, matchLabels[1].split(','))
-
-    labelsAll = convertTimestamp(labelsAll)
-    labelsCryptoCurrencySub = convertTimestamp(labelsCryptoCurrencySub)
-
-    return dataAll, dataCryptoCurrencySub, labelsAll, labelsCryptoCurrencySub
+import controllers.webScrapperController as wsc
 
 def main():
+
+    scrapData = wsc.WebScrapperController()
     crypto = 'LRC'
-    dataAll, dataCryptoCurrencySub, labelsAll, labelsCryptoCurrencySub = getRedditCryptoData(crypto)
+    dataAll, dataCryptoCurrencySub, labelsAll, labelsCryptoCurrencySub = scrapData.getRedditCryptoData(crypto)
 
     df = pd.DataFrame({'All': dataAll, 'CryptoCurrencySub': dataCryptoCurrencySub}, index=labelsAll)
+    df['MA_18'] = df.All.rolling(18, min_periods=1).mean() #Média móvel de 180 minutos
     df.plot()
     plt.show()
 
