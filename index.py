2aff9d6eff2a80b4372fb89ebdb46da461972b73
Guilherme Ely <guilherme.ely.ferreira@gmail.com>
2021-11-19 16:43:39 -0300

    saving into a database

--- index.py
+++ index.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import controllers.webScrapperController as wsc
+import sqlite3
 
 def main():
 
@@ -8,9 +9,21 @@ def main():
     crypto = 'LRC'
     dataAll, dataCryptoCurrencySub, labelsAll, labelsCryptoCurrencySub = scrapData.getRedditCryptoData(crypto)
 
-    df = pd.DataFrame({'All': dataAll, 'CryptoCurrencySub': dataCryptoCurrencySub}, index=labelsAll)
-    df['MA_18'] = df.All.rolling(18, min_periods=1).mean() #Média móvel de 180 minutos
-    df.plot()
-    plt.show()
+    dftodb = pd.DataFrame(zip(labelsAll,dataAll), columns=['DATE','MENTIONS'])
+    dftodb['CRYPTO'] = crypto
+
+    print(dftodb)
+
+    # df = pd.DataFrame({'MentionsAllSubs': dataAll, 'MentionsCryptoCurrSub': dataCryptoCurrencySub}, index=labelsAll)
+    # print(df)
+    
+    # df['MA_18'] = df.MentionsAllSubs.rolling(18, min_periods=1).mean() #Média móvel de 180 minutos
+    # print(df)
+    # df.plot()
+    # plt.show()
+
+    conn = sqlite3.connect(r"sqlite\scrap.db")
+    #not working properly
+    dftodb.to_sql('ScrappedMentionsData', conn, if_exists='append')
 
 main()
